0635353e3b3cdf4b9e40df7d70c56f38428d6e59
Sergїi Nesterchuk <nesterchuk911@gmail.com>
2025-06-11 16:28:20 +0300

    add: проект працює, підключено TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-yellow-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-green-700">
+        Tailwind Працює! 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
